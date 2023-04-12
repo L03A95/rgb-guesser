b3c163c95528813bba7e5579544c22931c538298
Liam M. Perez Lupia <liamperezlupia53@gmail.com>
2023-04-12 14:44:50 -0300

    main_screen

---
+++ src/Components/Game.jsx
@@ -0,0 +1,12 @@
+import MainScreen from "./Titles/MainScreen";
+
+
+
+export default function Game () {
+
+    return (
+        <>
+            <MainScreen></MainScreen>
+        </>
+    )
+}
---
+++ src/Components/Titles/MainScreen.jsx
@@ -0,0 +1,13 @@
+import "../../Styles/MainScreen.css"
+
+
+export default function MainScreen () {
+
+    return (
+        <>
+            <button className="main_buttons">Play</button>
+            <button className="main_buttons">About</button>
+            <h3>High Score: 0</h3>
+        </>
+    )
+}
