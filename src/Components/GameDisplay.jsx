bee2b338add52b2f34c3d89818cb49a0c4a77892
Liam M. Perez Lupia <liamperezlupia53@gmail.com>
2023-04-16 15:45:40 -0300

    function_game

--- src/Components/GameDisplay.jsx
+++ src/Components/GameDisplay.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react"
+import "../Styles/Game.css"
 
 
 export default function GameDisplay ({gameShowHandler}) {
@@ -21,10 +22,43 @@ export default function GameDisplay ({gameShowHandler}) {
         return hex.length == 1 ? "0" + hex : hex;
       }
 
+      function options(color) {
+        let resul = []
+        let num = Math.floor(Math.random() * 6);
+
+        for (let i = 0; i < 6; i++) {
+          let random = getRandomColor()
+          i === num ? resul.push(<h5 style={{height:"40px",width:"230px",margin:"80px"}}>{color.rgb}  {color.hex}</h5>) : resul.push(<h5 style={{height:"40px",width:"230px",margin:"80px"}}>{random.rgb}  {random.hex}</h5>)
+        }
+        return resul
+      }
+
+      function game () {
+        let colorCorrecto = getRandomColor()
+
+        let styleColor = {
+          height: "150px",
+          width: "1400px",
+          marginLeft:"65px",
+          backgroundColor: colorCorrecto.rgb
+        }
+
+        return (
+          <div>
+            <div style={styleColor}></div>
+            <div className="option-container">
+              {options(colorCorrecto).map((e) => e)}
+            </div>
+          </div>
+        )
+      }
+
+
     return(
         <>
-            <strong onClick={() => gameShowHandler(false,true)}>Main menu          </strong>
+            <strong onClick={() => gameShowHandler(false,true)}>Main menu</strong>
             <strong>Score: {score}</strong>
+            {game()}
         </>
     )
 }
